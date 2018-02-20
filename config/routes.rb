566d97ccc2f37d5e38adc140af18f934442d0e17
BenCmjn <benjamin.calmejane@gmail.com>
2018-02-20 14:39:27 +0100

    links created to two differents LP

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'home/checkr'
+  root'home#index'
 
+  get 'home/index'
+  get 'home/checkr'
   get 'home/flynn'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
